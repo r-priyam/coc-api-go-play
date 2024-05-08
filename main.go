6d152ac8c72b0f0802e4d5e33c00753e8f87c12d
r-priyam <roy_priyam@outlook.com>
2024-05-08 10:55:06 +0530

    push

--- main.go
+++ main.go
@@ -285,7 +285,15 @@ func main() {
 		Addr: config.RedisURL,
 		DB:   0,
 	})
-	mongo := MongoClient(config.MongoDbURL)
+	mongo, err := mongo.Connect(
+		context.TODO(),
+		options.Client().
+			ApplyURI(config.MongoDbURL).
+			SetServerAPIOptions(options.ServerAPI(options.ServerAPIVersion1)),
+	)
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB")
+	}
 
 	client := &http.Client{
 		Timeout: time.Second * 10,
